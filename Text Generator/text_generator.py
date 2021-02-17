8c84106aa20477c713d7c28eb0e5c5bb0f11523c
alexsystems55 <alexsystems@list.ru>
2021-02-17 22:35:14 +0600

    Update Text Generator project (stage 4).

--- Text Generator/text_generator.py
+++ Text Generator/text_generator.py
@@ -1,14 +1,14 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
+from random import choices
 from nltk.tokenize import regexp_tokenize
 from nltk import bigrams
 
 # Data input
-file_path = input("Enter text file name: ")
+file_path = input()
 tokens = []
 try:
     with open(file_path, "r", encoding="utf-8") as text_file:
-        for line in text_file:
-            tokens += regexp_tokenize(line, r"\S+")
+        tokens = regexp_tokenize(text_file.read(), r"\S+")
 except OSError as error:
     print(error)
 
@@ -16,21 +16,17 @@ except OSError as error:
 bigrams_ = list(bigrams(tokens))
 
 # Build Markov chain model
-heads = defaultdict(dict)
+heads = defaultdict(Counter)
 for head, tail in bigrams_:
-    if tail in heads[head]:
-        heads[head][tail] += 1
-    else:
-        heads[head][tail] = 1
+    heads[head][tail] += 1
 
-# Check
-while True:
-    head = input()
-    if head == "exit":
-        exit()
-    print(f"Head: {head}")
-    if head in heads:
-        for tail in heads[head]:
-            print(f"Tail: {tail}\tCount: {heads[head][tail]}")
-    else:
-        print("The requested word is not in the model. Please input another word.")
+# Generate text
+for sentence in range(10):
+    text = [choices(bigrams_)[0][0]]
+    start_word = text[0]
+    for step in range(9):
+        text += choices(
+            list(heads[start_word].keys()), list(heads[start_word].values())
+        )
+        start_word = text[-1]
+    print(" ".join(text))
