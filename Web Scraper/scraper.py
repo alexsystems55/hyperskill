6d4b918840139b3e8fbd931d322484a40502ac92
alexsystems55 <alexsystems@list.ru>
2021-03-27 13:24:46 +0600

    Black

--- Web Scraper/scraper.py
+++ Web Scraper/scraper.py
@@ -27,6 +27,7 @@ for page in range(1, pages_number + 1):
         mkdir(dir_name)
     except FileExistsError:
         pass
+
     pages_url = f"?page={page}" if page > 1 else ""
     idx_page = get_page(f"{BASE_URL}/nature/articles{pages_url}")
     soup = BeautifulSoup(idx_page, "lxml")
@@ -35,9 +36,12 @@ for page in range(1, pages_number + 1):
         article_type = article.find(attrs={"data-test": "article.type"}).span.string
         if article_type == type_filter:
             file_name = dir_name + "\\"
-            file_name += str(article.h3.a.string).translate(str.maketrans(translation_table))
+            file_name += str(article.h3.a.string).translate(
+                str.maketrans(translation_table)
+            )
             file_name += ".txt"
             print(f"\tSaving file: {file_name}")
+
             article_link = article.h3.a.get("href")
             article_content = get_page(f"{BASE_URL}{article_link}")
             article_soup = BeautifulSoup(article_content, "lxml")
